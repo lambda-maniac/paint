ac84473a5ee19432517b02b1e91625a8df15cc60
Lord Zarkares <73501736+LordZarkares@users.noreply.github.com>
2021-04-04 08:52:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Black Background and red text.
+#Syntax: [color], a "-" before the color name would make it colorize the background.
+cprint("[-black][red]Warning!")
+
+#Black Background and red text(RGB).
+#Syntax: [R, G, B], a "-" before the R would make it colorize the background.
+cprint("[- 40, 40, 40][251, 73, 52]Warning!")
+
+#Some styling too.
+#Syntax: [style].
+cprint("[-40, 40, 40][red][underlined][bold]Warning!")
